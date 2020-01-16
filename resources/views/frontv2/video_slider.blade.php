80f9cd105d1e7246caf0eb790018489ae5fcef5e
yousefashrafi <yousef.ashraf@ivas.com.eg>
2020-01-16 10:57:15 +0200

    base

--- resources/views/frontv2/video_slider.blade.php
+++ resources/views/frontv2/video_slider.blade.php
@@ -3,42 +3,40 @@
     <div class="row">
       <div class="col-md-8 col-xl-8">
         <div class="owl-one fadeOut owl-carousel owl-theme" >
+          @foreach ($slides as $slide)
+              
           <div class="item">
-            <a href="listproduct.php">
-              <img class="d-block w-100 rounded" src="images/owl_carousel/slide-1.jpg" alt="First slide">
+            <a href="{{$slide->ads_url}}">
+              <img class="d-block w-100 rounded" src="{{$slide->image}}" alt="{{$slide->image}}">
             </a>
           </div>
-          <div class="item">
-            <a href="listproduct.php">
-              <img class="d-block w-100 rounded" src="images/owl_carousel/car.jpg" alt="First slide">
-            </a>
-          </div>
-          <div class="item">
-            <a href="listproduct.php">
-              <img class="d-block w-100 rounded" src="images/owl_carousel/slide-3.jpg" alt="First slide">
-            </a>
-          </div>
-          <div class="item">
-            <a href="listproduct.php">
-              <img class="d-block w-100 rounded" src="images/owl_carousel/slide-3.webp" alt="First slide">
-            </a>
-          </div>
+          
+          @endforeach
         </div>
       </div>
 
       <div class="col-md-4 col-xl-4 ads">
         <div class="row">
+          @if (count($ads) > 1)
+              
           <div class="col-md-12 col-xl-12 col-6 top_banner">
-            <a href="listproduct.php">
-              <img class="rounded w-100" src="images/ads/top-banner.jpg" alt="Cash Offers">
+            <a href="{{$ads[0]->ads_url}}">
+              <img class="rounded w-100" src="{{$ads[0]->image}}" alt="{{$ads[0]->ads_url}}">
+            </a>
+          </div>
+          
+          @endif
+
+          @if (count($ads) > 2)
+
+          <div class="col-md-12 col-xl-12 col-6 top_banner bottom_banner mt-3">
+            <a href="{{$ads[1]->ads_url}}">
+              <img class="rounded w-100" src="{{$ads[1]->image}}" alt="{{$ads[1]->ads_url}}">
             </a>
           </div>
 
-          <div class="col-md-12 col-xl-12 col-6 top_banner bottom_banner mt-3">
-            <a href="#">
-              <img class="rounded w-100" src="images/ads/bottom-banner.jpg" alt="Pay your installment online">
-            </a>
-          </div>
+          @endif
+
         </div>
       </div>
     </div>
