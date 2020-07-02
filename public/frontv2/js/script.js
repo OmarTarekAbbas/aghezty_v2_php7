3a70d029431dc984fc2dbd0b114904ff3ad4cc45
Baher Talal <baher@ivas.com.eg>
2020-07-02 16:41:26 +0200

    fix list page

--- public/frontv2/js/script.js
+++ public/frontv2/js/script.js
@@ -7,9 +7,6 @@ $(window).scroll(function () {
   }
 });
 
-
-
-
 $('#return-to-top').click(function () { // When arrow is clicked
   $('body,html').animate({
     scrollTop: 0 // Scroll to top of body
