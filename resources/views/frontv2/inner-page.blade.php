6b5d45047c95d7c7c4e5873026effea50540ccbe
emad.mohamed.ivas <emad@ivas.com.eg>
2020-07-18 14:32:36 +0200

    emad,merge baher and edit

--- resources/views/frontv2/inner-page.blade.php
+++ resources/views/frontv2/inner-page.blade.php
@@ -399,7 +399,7 @@
       <div class="col-md-3 col-6 mb-3">
         <div class="content_view hvr-bob px-2 h-100 bg-white rounded">
           <a href="{{route('front.home.inner',['id' => $item->id])}}">
-            <img src="{{url($item->main_image)}}" alt="Product" class="w-100 based_selection_img">
+            <img  width="202" height="190"  src="{{url($item->main_image)}}" alt="Product" >
 
             @if($item->discount > 0)
             <div class="product-label text-center font-weight-bold">
