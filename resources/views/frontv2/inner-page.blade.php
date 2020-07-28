04faf620dcf310743783f1a48fbf9da6c17da377
Baher Talal <baher@ivas.com.eg>
2020-07-28 11:27:59 +0200

    fix img zoom

--- resources/views/frontv2/inner-page.blade.php
+++ resources/views/frontv2/inner-page.blade.php
@@ -76,7 +76,7 @@
                 <div class="carousel-item {{$key == 0 ?'active' : ''}}">
                   <div class="easyzoom easyzoom--overlay easyzoom--with-thumbnails">
                     <a class="zoom_image" href="{{url($image->image)}}">
-                      <img width="500px" height="{{$product->category->id == 17 ? '700px' : '600px'}}" src="{{url($image->image)}}" alt="{{$product->getTranslation('title',getCode())}}" />
+                      <img class="m-auto d-block text-center" width="500px" height="{{$product->category->id == 17 ? '600px' : '400px'}}" src="{{url($image->image)}}" alt="{{$product->getTranslation('title',getCode())}}" />
                     </a>
                   </div>
                 </div>
