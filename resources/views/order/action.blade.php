74dcb0cf6e05e3432a84d1c57a8d529bf6fe2cc3
omar tarek <omar.tarek@iVAS.local>
2020-11-18 15:28:42 +0200

    make delete enable_testing order

--- resources/views/order/action.blade.php
+++ resources/views/order/action.blade.php
@@ -2,5 +2,7 @@
   @if(count($order->products) > 0)
     <a class="btn btn-sm show-tooltip" title="@lang('messages.show_product')" href="{{url("order/".$order->id)}}" data-original-title="show Products"><i class="fa fa-step-forward"></i></a>
   @endif
+  @if (setting('enable_delete'))
   <a class="btn btn-sm show-tooltip btn-danger" onclick="return ConfirmDelete();" href="{{url("order/$order->id/delete")}}" title="@lang('messages.template.delete')"><i class="fa fa-trash"></i></a>
+  @endif
 </td>
