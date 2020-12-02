2c1d77d41fa5aa475479fd99f9968f3017254f2b
emad.mohamed.ivas <emad@ivas.com.eg>
2020-12-02 18:02:33 +0200

    emad,comment fb

--- resources/views/frontv2/auth/register.blade.php
+++ resources/views/frontv2/auth/register.blade.php
@@ -115,10 +115,10 @@
               <div class="col-md-12 col-lg-12 col-xl-12 col-12">
                 <button type="submit" class="btn_save btn btn-secondary text-white mb-1 m-auto d-block w-50 text-capitalize hvr-wobble-to-bottom-right">@lang('front.auth.register')</button>
               </div>
-
+{{--
               <div class="col-md-12 col-lg-12 col-xl-12 col-auto">
                 <a href="{{url('/facebook_redirect')}}" class="btn_log_fb btn m-auto d-block text-capitalize"><i class="fab fa-facebook-f"></i> @lang('front.auth.facebook')</a>
-              </div>
+              </div>  --}}
           </div>
           </div>
         </form>
