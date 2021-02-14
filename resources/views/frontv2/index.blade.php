b5647cb20c5cea556a61de9caf95be18490d2ba5
mohamed mahmoud <m.mahmoud@iVAS.local>
2021-02-14 17:14:23 +0200

    fix set slug

--- resources/views/frontv2/index.blade.php
+++ resources/views/frontv2/index.blade.php
@@ -435,7 +435,7 @@
           @foreach ($recently_added as $item)
           <div class="col-md-4 col-xl-2 col-6 margin_bottom_mob">
             <div class="px-2 product_desc hvr-bob rounded">
-            <a class="m-1" href="{{route('front.home.inner',['id' => $item->id ,'category_name' => setcategory_name($item->getTranslation('title',getCode()))])}}">
+            <a class="m-1" href="{{route('front.home.inner',['id' => $item->id ,'category_name' => setSlug($item->getTranslation('title',getCode()))])}}">
                 <img src="{{$item->main_image}}" alt="{{$item->getTranslation('title',getCode())}}"
                   class="text-center d-block">
 
@@ -550,7 +550,7 @@
 
           <div class="col-md-4 col-xl-2 col-6 margin_bottom_mob">
             <div class="px-2 product_desc hvr-bob rounded">
-            <a class="m-1" href="{{route('front.home.inner',['id' => $item->id ,'category_name' => setcategory_name($item->getTranslation('title',getCode()))])}}">
+            <a class="m-1" href="{{route('front.home.inner',['id' => $item->id ,'category_name' => setSlug($item->getTranslation('title',getCode()))])}}">
                 <img src="{{$item->main_image}}" alt="{{$item->getTranslation('title',getCode())}}"
                   class="img_size d-block">
 
