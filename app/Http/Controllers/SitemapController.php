b74bd148df53096ca498565dfda7843eadf2a65d
omar tarek <omar.tarek@iVAS.local>
2021-02-16 09:51:50 +0200

    make orderby

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -14,7 +14,7 @@ class SitemapController extends Controller
 
   public function sitemap_categories()
   {
-    $categories = Category::whereNotNull("parent_id")->get();
+    $categories = Category::whereNotNull("parent_id")->orderBy('updated_at', 'DESC')->get();
     return response(view('sitemap.categories')->with(compact('categories')), 200, [
       'Content-Type' => 'test/xml',
       'Content-Disposition' => 'attachment; filename="categories.xml"',
@@ -23,7 +23,7 @@ class SitemapController extends Controller
 
   public function sitemap_products()
   {
-    $products = Product::whereDate('updated_at',Carbon::now()->toDateString())->get();
+    $products = Product::whereDate('updated_at',Carbon::now()->toDateString())->orderBy('updated_at', 'DESC')->get();
     return response(view('sitemap.products')->with(compact('products')), 200, [
       'Content-Type' => 'test/xml',
       'Content-Disposition' => 'attachment; filename="products.xml"',
@@ -32,7 +32,7 @@ class SitemapController extends Controller
 
   public function image_products()
   {
-    $image_products = ProductImage::whereDate('updated_at',Carbon::now()->toDateString())->get();
+    $image_products = ProductImage::whereDate('updated_at',Carbon::now()->toDateString())->orderBy('updated_at', 'DESC')->get();
     return response(view('sitemap.ImageProducts')->with(compact('image_products')), 200, [
       'Content-Type' => 'test/xml',
       'Content-Disposition' => 'attachment; filename="ImageProducts.xml"',
