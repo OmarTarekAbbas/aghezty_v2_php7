b00dd9cf4d267f5b8b62f8811b6a01c8785b2728
unknown <Omer.tarek@ivas.com.eg>
2021-03-03 13:34:38 +0200

    check filter with random to show sub cats like offers

--- app/helpers.php
+++ app/helpers.php
@@ -523,6 +523,7 @@ function checkbuyLimit($product_id)
     $countOrder = OrderDetail::where("product_id", $product_id)->whereHas("order",function($query) {
       $query->where("client_id", auth()->guard("client")->id());
     })->sum("quantity");
+    
     if($countOrder < 2) {
       return ['status' => true ,'count' => $countOrder];
     }
