a3b8648392a182b6b7c00bbd94f1b4c018675eb7
emad.mohamed.ivas <emad@ivas.com.eg>
2021-03-24 17:34:26 +0200

    emad,edit

--- app/Http/Controllers/ImageResizeController.php
+++ app/Http/Controllers/ImageResizeController.php
@@ -56,12 +56,51 @@ class ImageResizeController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    
+
     public function resizeProductImage()
     {
-      dispatch(new ResizeImage());
+     // dispatch(new ResizeImage());
+
+     ini_set('memory_limit', '20M');
+
+     $path = 'uploads/product/image_resize';
+       $destinationPath = base_path($path);
+
+     if (!File::exists($path)) {
+       File::makeDirectory($path, 0755, true, true);
+     }
+
+       $products = Product::whereNull('main_image_resize')->orderBy("id", "desc")->get();
+     foreach ($products as $product) {
+       $image = $product->main_image;
+       if(isset($image) && $image!=null){
+       $image_resize_path = $destinationPath . '/' . $product->id . ".webp";
+       //resize image
+
+       $ext = pathinfo($image, PATHINFO_EXTENSION);
+       if ($ext != "png") {
+       $img = Image::make($image);
+       $img->encode('webp', 90)->save($image_resize_path);
+       } elseif($ext == "png") {
+       $image_form = imagecreatefrompng($image);
+       imagepalettetotruecolor($image_form);
+       imageAlphaBlending($image_form, true); // alpha channel
+       imageSaveAlpha($image_form, true); // save alpha setting
+
+       $img = Image::make($image_form);
+       $img->encode('webp', 90)->save($image_resize_path);
+       }
+
+       //save image
+       $product->main_image_resize = $path . '/' . $product->id . ".webp";
+       $product->save();
+     }
+     }
+
+     echo "Products Resized Done" ;
+
     }
-    
+
 
     public function resizeProductImages(){
       $path = 'uploads/product/image_resize';
