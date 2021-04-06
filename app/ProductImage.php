e39468144dc63092cd1351d01020d862ac809dc4
mahmoud maher <mahmoud.maher@ivas.com.eg>
2021-04-06 17:24:32 +0200

    update excel image

--- app/ProductImage.php
+++ app/ProductImage.php
@@ -18,16 +18,17 @@ class ProductImage extends Model
     {
         $path     = '/uploads/product/'.date('Y-m-d').'/';
 
-        if(strpos($value,'uploads/product') !== false)
-        {
-          $this->attributes['image'] = $value;
-        }
+        // if(strpos($value,'uploads/product') !== false)
+        // {
+        //   $this->attributes['image'] = $value;
+        // }
 
-        if(is_file($value))
-        {
+        if(is_file($value)){
         $img_name = time().rand(0,999).'.'.$value->getClientOriginalExtension();
         $value->move(base_path($path),$img_name);
         $this->attributes['image']= $path.$img_name ;
+        }else{
+          $this->attributes['image']= $path.$value ;
         }
 
     }
